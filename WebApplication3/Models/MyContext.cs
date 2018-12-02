cd7ce488c456e1d4418ca6861de02f47999eb1f2
lukx <admin@lukx.net>
2018-12-02 20:38:25 +0100

    adssdaadsasd

---
+++ WebApplication3/Models/MyContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Osoba> Osoby { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ WebApplication3/Models/Osoba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Osoba
+    {
+        public int OsobaId { get; set; }
+
+        public string Jmeno { get; set; }
+
+        public string Prijmeni { get; set; }
+
+        public int Vek { get; set; }
+    }
+}
